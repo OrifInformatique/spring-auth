7adcd3e78e7842045f05698f7e4ab41d0b7904cc
KenCacciabueOrif <ken.cacciabue@formation.orif.ch>
2025-10-28 08:59:59 +0100

    feat: removed useless condition in jwtauthfilter

--- src/main/java/ch/sectioninformatique/auth/security/JwtAuthFilter.java
+++ src/main/java/ch/sectioninformatique/auth/security/JwtAuthFilter.java
@@ -80,13 +80,9 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             if (authElements.length == 2
                     && "Bearer".equals(authElements[0])) {
                 try {
-                    if ("GET".equals(request.getMethod())) {
-                        SecurityContextHolder.getContext().setAuthentication(
-                                userAuthenticationProvider.validateToken(authElements[1]));
-                    } else {
-                        SecurityContextHolder.getContext().setAuthentication(
-                                userAuthenticationProvider.validateTokenStrongly(authElements[1]));
-                    }
+
+                    SecurityContextHolder.getContext().setAuthentication(
+                            userAuthenticationProvider.validateTokenStrongly(authElements[1]));
                 } catch (JWTVerificationException e) {
                     SecurityContextHolder.clearContext();
                     log.debug("Invalid JWT token: {}", e.getMessage());
