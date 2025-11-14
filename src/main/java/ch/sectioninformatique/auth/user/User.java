87489cc6bb7484d390f71cc1c7bf9df5c13d6406
Didier Viret <didier.viret@orif.ch>
2025-11-14 15:22:26 +0100

    refactor: remove unused imports

--- src/main/java/ch/sectioninformatique/auth/user/User.java
+++ src/main/java/ch/sectioninformatique/auth/user/User.java
@@ -11,7 +11,6 @@ import org.hibernate.annotations.FilterDef;
 import org.hibernate.annotations.ParamDef;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.hibernate.annotations.Where;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
