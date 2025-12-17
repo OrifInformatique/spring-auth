721e030a18381189148b53925e9ce50899a9d095
Didier Viret <didier.viret@orif.ch>
2025-12-17 15:22:50 +0100

    build: revert last commit, use MockitoBean instead of MockBean

--- src/test/java/ch/sectioninformatique/auth/user/UserControllerDocTest.java
+++ src/test/java/ch/sectioninformatique/auth/user/UserControllerDocTest.java
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -43,7 +44,6 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAu
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
@@ -74,31 +74,31 @@ class UserControllerDocTest {
         private MockMvc mockMvc;
 
         /** Mocked UserService for simulating user-related operations */
-        @MockBean
+        @MockitoBean
         private UserService userService;
 
         /** Mocked UserMapper for simulating user-related mapping operations */
-        @MockBean
+        @MockitoBean
         private UserMapper userMapper;
 
         /** Mocked RoleRepository for simulating role-related database operations */
-        @MockBean
+        @MockitoBean
         private RoleRepository roleRepository;
 
         /** Mocked UserRepository for simulating user-related database operations */
-        @MockBean
+        @MockitoBean
         private UserRepository userRepository;
 
         /**
          * Mocked UserAuthenticationProvider for simulating authentication operations
          */
-        @MockBean
+        @MockitoBean
         private UserAuthenticationProvider userAuthenticationProvider;
 
-        @MockBean
+        @MockitoBean
         private Authentication authentication;
 
-        @MockBean
+        @MockitoBean
         private SecurityContext securityContext;
 
         private static String meResponseJson;
