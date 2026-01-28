acf98dc261ec6668f9afcbfddf3b84cad6d3f743
KenCacciabueOrif <ken.cacciabue@formation.orif.ch>
2026-01-28 11:33:19 +0100

    test: modified tests to check password in repository after register (#21)

--- src/test/java/ch/sectioninformatique/auth/user/UserServiceTest.java
+++ src/test/java/ch/sectioninformatique/auth/user/UserServiceTest.java
@@ -96,7 +96,7 @@ public class UserServiceTest {
         UserDto expectedDto = new UserDto(1L, "John", "Doe", login, null, false, "USER", null);
         
         when(userRepository.findByLogin(login)).thenReturn(Optional.of(user));
-        when(passwordEncoder.matches(CharBuffer.wrap(password), user.getPassword())).thenReturn(true);
+        when(passwordEncoder.matches(password, user.getPassword())).thenReturn(true);
         when(userMapper.toUserDto(user)).thenReturn(expectedDto);
 
         // Act
@@ -105,7 +105,7 @@ public class UserServiceTest {
         // Assert
         assertEquals(expectedDto, result);
         verify(userRepository).findByLogin(login);
-        verify(passwordEncoder).matches(CharBuffer.wrap(password), user.getPassword());
+        verify(passwordEncoder).matches(password, user.getPassword());
     }
 
     /**
@@ -159,7 +159,7 @@ public class UserServiceTest {
         User user = new User(1L, "John", "Doe", login, "hashedPassword", null, null,false, null);
         
         when(userRepository.findByLogin(login)).thenReturn(Optional.of(user));
-        when(passwordEncoder.matches(CharBuffer.wrap(password), user.getPassword())).thenReturn(false);
+        when(passwordEncoder.matches(password, user.getPassword())).thenReturn(false);
 
         // Act & Assert
         AppException exception = assertThrows(AppException.class, 
@@ -209,7 +209,7 @@ public class UserServiceTest {
         userRole.setName(RoleEnum.USER);
         
         when(userRepository.findByLogin(login)).thenReturn(Optional.empty());
-        when(passwordEncoder.encode(CharBuffer.wrap(password))).thenReturn("hashedPassword");
+        when(passwordEncoder.encode(password)).thenReturn("hashedPassword");
         when(roleRepository.findByName(RoleEnum.USER)).thenReturn(Optional.of(userRole));
         when(userMapper.signUpToUser(signUpDto)).thenReturn(user);
         when(userRepository.save(user)).thenReturn(user);
@@ -221,7 +221,7 @@ public class UserServiceTest {
         // Assert
         assertEquals(expectedDto, result);
         verify(userRepository).findByLogin(login);
-        verify(passwordEncoder).encode(CharBuffer.wrap(password));
+        verify(passwordEncoder).encode(password);
         verify(roleRepository).findByName(RoleEnum.USER);
         verify(userRepository).save(user);
     }
