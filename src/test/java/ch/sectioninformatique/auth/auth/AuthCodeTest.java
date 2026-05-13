2a3253473e70c37b92895a5db7925ec0a64af82a
MoQu <Quentin.Mory@formation.orif.ch>
2026-05-13 08:09:39 +0200

    fix: fix the tests for the AuthCodeTest class

--- src/test/java/ch/sectioninformatique/auth/auth/AuthCodeTest.java
+++ src/test/java/ch/sectioninformatique/auth/auth/AuthCodeTest.java
@@ -1,9 +1,11 @@
 package ch.sectioninformatique.auth.auth;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import ch.sectioninformatique.auth.user.User;
 import ch.sectioninformatique.auth.user.UserRepository;
+import jakarta.transaction.Transactional;
 
 /** 
  * Test class for {@link AuthCode}.
@@ -24,11 +27,11 @@ import ch.sectioninformatique.auth.user.UserRepository;
 @SpringBootTest
 public class AuthCodeTest {
     
-    private final Long TEST_codeId = 1L;
     private final String TEST_code = "ThisIsATestCode";
     private final String TEST_userLogin = "test.admin@test.com";
     private final Instant TEST_expiresAt = Instant.now().plusSeconds(30); // Expires in 30 seconds
     private final Instant TEST_createdAt = Instant.now();
+    private final String TEST_redirectURL = "NotARealURL";
 
     @Autowired
     private AuthCodeRepository authCodeRepository;
@@ -36,30 +39,51 @@ public class AuthCodeTest {
     @Autowired
     private UserRepository userRepository;
 
+    /**
+     * Test : Create a new code, then stock in the database and finally, retrieves it.
+     * 
+     * Test data:
+     * -AuthCode
+     */
     @Test
     void createAuthCode() {
+        authCodeRepository.deleteAll();
+        Instant now = Instant.now();
+        Instant expiredAt = Instant.now().plusSeconds(30);
+
         AuthCode authCode = new AuthCode();
-        authCode.setId(TEST_codeId);
         authCode.setCode(TEST_code);
         authCode.setUserLogin(TEST_userLogin);
-        authCode.setExpiresAt(TEST_expiresAt);
-        authCode.setCreatedAt(TEST_createdAt);
+        authCode.setRedirectUrl(TEST_redirectURL);
+        authCode.setExpiresAt(expiredAt);
+        authCode.setCreatedAt(now);
+        authCodeRepository.save(authCode);
 
-        assert authCode.getId().equals(TEST_codeId);
-        assert authCode.getCode().equals(TEST_code);
-        assert authCode.getUserLogin().equals(TEST_userLogin);
-        assert authCode.getExpiresAt().equals(TEST_expiresAt);
-        assert authCode.getCreatedAt().equals(TEST_createdAt);
+        Optional<AuthCode> retrievedAuthCode = authCodeRepository.findById(authCode.getId()); 
 
+
+        assertEquals(TEST_userLogin, retrievedAuthCode.get().getUserLogin());
+        assertEquals(TEST_code, retrievedAuthCode.get().getCode());
+        assertEquals(now.truncatedTo(ChronoUnit.MICROS), retrievedAuthCode.get().getCreatedAt().truncatedTo(ChronoUnit.MICROS));
+        assertEquals(expiredAt.truncatedTo(ChronoUnit.MICROS), retrievedAuthCode.get().getExpiresAt().truncatedTo(ChronoUnit.MICROS));
+        assertEquals(TEST_redirectURL, retrievedAuthCode.get().getRedirectUrl());        
     }
 
+    /**
+     * Test : Create 2 codes for a user, then stock them, and finally, retrieve them.
+     * Verify that the list contains 2 Elements
+     * 
+     * Test Data:
+     * - 2 AuthCode
+     */
     @Test
     void getCodeByUserLogin() {
-
+        authCodeRepository.deleteAll();
         AuthCode authCode = new AuthCode();
         authCode.setCode(TEST_code);
         authCode.setUserLogin(TEST_userLogin);
         authCode.setExpiresAt(TEST_expiresAt);
+        authCode.setRedirectUrl(TEST_redirectURL);;
         authCode.setCreatedAt(TEST_createdAt);
         authCodeRepository.save(authCode);
 
@@ -67,6 +91,7 @@ public class AuthCodeTest {
         secondCode.setCode(TEST_code + ".2");
         secondCode.setUserLogin(TEST_userLogin);
         secondCode.setExpiresAt(TEST_expiresAt);
+        secondCode.setRedirectUrl(TEST_redirectURL);
         secondCode.setCreatedAt(TEST_createdAt);
         authCodeRepository.save(secondCode);
 
@@ -74,33 +99,52 @@ public class AuthCodeTest {
         System.out.println("Retrieved auth codes for user " + TEST_userLogin + ": " + retrievedAuthCode.size());
 
         
-        assert !retrievedAuthCode.isEmpty();
+        assertTrue(!retrievedAuthCode.isEmpty());
+        assertEquals(2, retrievedAuthCode.size());
     }
-assert authCode.getId().equals(TEST_codeId);
+
+    /**
+     * Test : Getting codes from a user without codes.
+     * Verify that the method doesn't throw an error
+     * 
+     * Test Data:
+     * - user's login
+     */
     @Test
     void getAuthCodeFromUserWithNoCode() {
         Optional<User> user = userRepository.findByLogin("test.user@test.com");
         List<AuthCode> authCode = authCodeRepository.findByUserLogin(user.get().getLogin());
 
-        assert authCode.isEmpty();
+        assertTrue(authCode.isEmpty());
     }
 
+    /**
+     * Test : Delete expired codes
+     * Verify that expired codes are deleted
+     * 
+     * Test Data:
+     * - expired code
+     */
     @Test
+    @Transactional
     void deleteExpiredCodes() {
+        authCodeRepository.deleteAll();
         AuthCode authCode = new AuthCode();
         authCode.setCode("ExpiredTestCode");
         authCode.setUserLogin("test.manager@test.com");
+        authCode.setRedirectUrl(TEST_redirectURL);
         authCode.setExpiresAt(Instant.now().minusSeconds(5)); 
         authCode.setCreatedAt(Instant.now());
         
         authCodeRepository.save(authCode);
 
-        
+        List<AuthCode> retrievedBeforeDelete = authCodeRepository.findByUserLogin(authCode.getUserLogin());
 
         authCodeRepository.deleteExpiredCodes();
 
         List<AuthCode> retrievedAuthCodes = authCodeRepository.findByUserLogin("test.manager@test.com");
 
+        assertTrue(!retrievedBeforeDelete.isEmpty());
         assertTrue(retrievedAuthCodes.isEmpty(), "Expired authentication code should have been deleted but get : " + retrievedAuthCodes.size());
 
 
