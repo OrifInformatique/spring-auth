8bc2a700a26a381053a9f0f8592d3c04a21a7ddd
MoQu <Quentin.Mory@formation.orif.ch>
2026-09-01 08:45:27 +0200

    feat: admin can't revoke their own role

--- src/main/java/ch/sectioninformatique/auth/user/UserController.java
+++ src/main/java/ch/sectioninformatique/auth/user/UserController.java
@@ -230,7 +230,16 @@ public class UserController {
      */
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{login}/downgrade-admin")
-    public ResponseEntity<?> downgradeAdminRole(@PathVariable String login) {
+    public ResponseEntity<?> downgradeAdminRole(@PathVariable String login, @AuthenticationPrincipal UserDto currentUser) {
+
+        if (currentUser.getLogin().equals(login)) {
+            return ResponseEntity.badRequest().body(messageSource.getMessage(
+                    "message.user.downgrade.self",
+                    null,
+                    LocaleContextHolder.getLocale()
+            ));
+        }
+
         userService.downgradeAdminRole(login);
         return ResponseEntity.ok().body(messageSource.getMessage(
                 "message.user.downgraded.admin",
